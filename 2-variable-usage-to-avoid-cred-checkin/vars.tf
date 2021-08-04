2a3540749f1c511afab057808805e12dd4ef4901
Pankhudi <bpankhudi7@gmail.com>
2021-08-04 12:43:49 +0530

    Variable usage + How to avoid checking-in the aws credential vars.tf -> to declare variables terraform.tfvars -> has actual values provider.tf and instance.tf -> are levaraging variables

---
+++ 2-variable-usage-to-avoid-cred-checkin/vars.tf
@@ -0,0 +1,16 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    eu-west-1 = "ami-844e0bf7"
+  }
+}
