74857e79e6e622c1e52506c9c0b23a31a8faed77
Pankhudi <bpankhudi7@gmail.com>
2021-09-01 10:20:22 +0530

    tf variables validation

--- terroform-with-docker/main.tf
+++ terroform-with-docker/main.tf
@@ -9,6 +9,11 @@ terraform {
 variable "int_port" {
   type = number
   default = 1880
+
+  validation {
+    condition = var.int_port <= 65535 && var.int_port > 0
+    error_message = "The port must be in a valid port range 0 - 65535."
+  }
 }
 
 provider "docker" {}
