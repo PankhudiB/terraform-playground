17a4c4d5a92fdc3fc76fb349f1233550c0dfecf6
Pankhudi <bpankhudi7@gmail.com>
2021-09-03 09:35:14 +0530

    extract image module

--- terroform-with-docker/main.tf
+++ terroform-with-docker/main.tf
@@ -1,7 +1,5 @@
-provider "docker" {}
-
-resource "docker_image" "nodered_image" {
-  name = "nodered/node-red:latest"
+module "image" {
+  source = "./image"
 }
 
 resource "random_string" "random" {
@@ -13,7 +11,7 @@ resource "random_string" "random" {
 
 resource "docker_container" "nodered_container" {
   count = 2
-  image = docker_image.nodered_image.latest
+  image = module.image.image_out
   name = join("-",["nodered",random_string.random[count.index].result])
   ports {
     internal = var.int_port
